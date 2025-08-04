ac34859b8ce2c48ffe476e6a0bc810fa3034c7d8
iivaniivan <yvan_kalbermatten@hotmail.com>
2025-08-04 17:06:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -123,4 +123,4 @@ fig.update_xaxes(
 fig.update_yaxes(title="Kontostand (CHF)")
 fig.update_layout(height=500)
 
-    st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig, use_container_width=True)
